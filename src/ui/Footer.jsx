033777610e7874862f8283b0efe9918155ed1f05
amitsuthar803 <amitsuthar803@gmail.com>
2024-06-06 17:31:06 +0530

    first commit

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <div className="h-[60px] border-t-2 flex justify-center m-auto border-b-2 items-center w-full bg-white">
       <div className=" w-[1111px] flex justify-between items-center">
-        Footer
+        Design & Developed by ©AmitSuthar
       </div>
     </div>
   );
