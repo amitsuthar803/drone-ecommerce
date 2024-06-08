bb66151f6759d075fde80d24c18fde320b32dc2f
amitsuthar803 <amitsuthar803@gmail.com>
2024-06-08 16:11:42 +0530

    shop updated

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,8 +1,80 @@
-import { FaSearch } from "react-icons/fa";
+import { useState } from "react";
 import { IoIosArrowRoundBack } from "react-icons/io";
 
 function Product() {
-  const category = ["fun", "agri", "industrial", "camera", "racing"];
+  const [selectedCategory, setSelectedCategory] = useState("all");
+
+  const category = [
+    "all",
+    "fun",
+    "agri",
+    "industrial",
+    "camera",
+    "racing",
+    "underwater",
+  ];
+
+  const droneImages = [
+    {
+      name: "drone1",
+      path: "/assets/drone1.png",
+      price: 999,
+      type: "fun",
+    },
+    {
+      name: "drone2",
+      path: "/assets/drone2.png",
+      price: 999,
+      type: "industrial",
+    },
+    {
+      name: "drone3",
+      path: "/assets/drone3.png",
+      price: 999,
+      type: "camera",
+    },
+    {
+      name: "drone4",
+      path: "/assets/drone4.png",
+      price: 999,
+      type: "camera",
+    },
+    {
+      name: "drone5",
+      path: "/assets/drone5.png",
+      price: 999,
+      type: "agri",
+    },
+    {
+      name: "drone6",
+      path: "/assets/drone6.png",
+      price: 999,
+      type: "agri",
+    },
+    {
+      name: "drone7",
+      path: "/assets/drone7.png",
+      price: 999,
+      type: "underwater",
+    },
+    {
+      name: "drone8",
+      path: "/assets/drone8.png",
+      price: 999,
+      type: "racing",
+    },
+    {
+      name: "drone9",
+      path: "/assets/drone9.png",
+      price: 999,
+      type: "industrial",
+    },
+  ];
+
+  const filterDrones =
+    selectedCategory === "all"
+      ? droneImages
+      : droneImages.filter((drone) => drone.type === selectedCategory);
 
   return (
     <div className="text-center flex-col flex m-auto w-full justify-center overflow-hidden">
@@ -24,16 +96,40 @@ function Product() {
 
       <div className="">
         <div className="flex flex-col items-center justify-start">
-          <div className="flex justify-center gap-2 items-center">
+          <div className="flex justify-center gap-2 flex-wrap  items-center">
             {category.map((cate) => (
               <span
                 key={cate}
-                className="px-[10px] hover:border-black hover:text-white hover:bg-black cursor-pointer capitalize max-sm:text-sm py-[1px] border-2 rounded-full border-gray-300"
+                onClick={() => setSelectedCategory(cate)}
+                className={`px-[10px] cursor-pointer capitalize max-sm:text-sm py-[1px] border-2 rounded-full border-gray-300
+                  ${
+                    selectedCategory === cate
+                      ? "bg-black text-white border-black"
+                      : " hover:border-black hover:text-white hover:bg-black"
+                  }
+                  `}
               >
                 {cate}
               </span>
             ))}
           </div>
+        </div>
+
+        <div className="grid grid-cols-2 md:grid-cols-3 mt-10 p-2 gap-5 overflow-y-scroll">
+          {filterDrones.map((drone) => (
+            <div
+              key={drone.name}
+              className="m-auto p-3 rounded-md bg-gray-100 flex flex-col"
+            >
+              <div className="py-5 rounded-md bg-white">
+                <img className="w-[250px] " src={`${drone.path}`} alt="" />
+              </div>
+              <div>
+                <h3>{drone.name}</h3>
+                <span>Price: {drone.price}</span>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
