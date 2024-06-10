57073a08874cd592cb58dc694aeca5daa51f6fd4
amitsuthar803 <amitsuthar803@gmail.com>
2024-06-10 22:03:14 +0530

    update

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useDroneData } from "../context/DroneContext";
 import { useState } from "react";
 import { IoCartOutline } from "react-icons/io5";
+import { FaMinus, FaPlus } from "react-icons/fa";
 
 function ProductDetail() {
   const { id } = useParams();
@@ -12,9 +13,13 @@ function ProductDetail() {
   const [selectProduct] = dronesData.filter((drone) => drone.id === Number(id));
 
   return (
-    <div className="flex w-full justify-between h-[100vh]  max-sm:flex-col lg:items-center">
+    <div className="flex w-full lg:mt-[3rem] justify-between h-[100vh]  max-sm:flex-col lg:items-start">
       <div className="flex-1 max-sm:rounded-t-[20px]  flex justify-center max-sm:items-center max-sm:bg-gradient-to-r from-red-500 to-orange-500 ">
-        <img src={selectProduct.path} alt="" />
+        <img
+          className="lg:h-[100%] md:h-[60%] sm:h-[50%]"
+          src={selectProduct.path}
+          alt=""
+        />
       </div>
 
       <div className=" flex-1 flex p-5 max-sm:bg-gray-100  mt-[-20px] max-sm:rounded-t-[20px] md:items-start justify-start flex-col">
@@ -26,30 +31,30 @@ function ProductDetail() {
         </h4>
 
         <div className="flex mt-3 items-center lg:text-md text-sm  gap-5">
-          <h4 className="font-semibold ">Quantity:</h4>
-          <div className="border-2 lg:py-1">
+          <h4 className="font-semibold text-gray-500">Quantity:</h4>
+          <div className="border-2 lg:py-0">
             <button
-              className="lg:px-5 px-2"
+              className="lg:px-4 px-2"
               onClick={() => setCount((count) => (count > 0 ? count - 1 : 0))}
             >
-              -
+              <FaMinus size={12} />
             </button>
             <input
               value={count}
               type="numeric"
-              className="lg:w-[80px] w-[50px] text-center py-1  border-x-2"
+              className="lg:w-[80px] bg-gray-200 font-semibold w-[50px] text-center py-2  border-x-2"
             ></input>
             <button
-              className="lg:px-5 px-2"
+              className="lg:px-4 px-2 items-center"
               onClick={() => setCount((count) => count + 1)}
             >
-              +
+              <FaPlus size={12} />
             </button>
           </div>
         </div>
 
         <div className="flex mt-5  lg:text-[1rem] text-sm gap-2">
-          <h4 className="font-semibold">Description:</h4>
+          <h4 className="font-semibold text-gray-500">Description:</h4>
           <p className="text-gray-500">{selectProduct.description}</p>
         </div>
 
